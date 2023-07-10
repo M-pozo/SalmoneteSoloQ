e29334e9ca4a890534bf88c19461f117cf8cca8b
M-pozo <117214588+M-pozo@users.noreply.github.com>
2023-07-10 23:54:22 +0200

    Update key

--- connectToApi.js
+++ connectToApi.js
@@ -26,7 +26,7 @@ const participantes = [
 let ordenSeleccionado = 'ascendente';
 
 // Clave de la API de Riot
-const key = 'RGAPI-4e124999-6402-4f8d-9feb-8c52b85f6d64';
+const key = 'RGAPI-3e8a8f4c-eea0-4a40-93b9-099a4b64489d';
 
 // Función para obtener los datos de un participante
 async function obtenerDatosParticipante(participante) {
