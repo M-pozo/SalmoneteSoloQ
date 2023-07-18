1ce0375caaa0ae08ea265565e1f363ae5dda1984
M-pozo <117214588+M-pozo@users.noreply.github.com>
2023-07-18 18:49:10 +0200

    Update key's

--- connectToApi.js
+++ connectToApi.js
@@ -1,26 +1,9 @@
 // Array de participantes
 const participantes = [
-  'BU3E8F7cR4LI30HYMBCn0-RKFeP0l0nzXICfaJ4VB0GxNn4'
-  //'y7X5vuP8TuNzzvJ3IOHmczbYHjQMXRh73-GgaPdsUfYhP70',
-  //'3eIR95u1l6PZDLmFwkqbOpWbxasu0bYaMcD2EY-YbzeZn2cm_uoXWqjJTw',
-  //'8R3HJ42tFYxYtCJ16jzPanhRMuoqBndk523gY4r7Owe0Oxtd',
-  //'acHKjfmlXMe_EKzjzyQwZcI9E4aFkhe-jp90XnZvIFc8M7Q',
-  //'AmqfQquQ4WLqfKNPr7CGx_RbX66_OfklGbw0i2bQ-wleKiHJ',
-  //'BNw9fVN6QMd0jR6DpdipWKgJZCc_-1qKl3l_gX1sdvALqXU',
-  //'kllZCAiVxG-9XLCj74j6nSxYZD4XkcGjjpEpF_NXop6osKOh',
-  //'qNs8UbyWy0KTKjbwciIqgbqNL3mACGHhf-nlHse3nUTi5Kqj',
-  //'zNLNuvnFxbqoPVKGWfdPQNieLkzo87vIBnRAh9pLK9GoMXw',
-  //'iwm0KpRPtbPQQCSZBm8LATzdzKT34nZ85h04bf_QG19HdkU',
- // 'QZ8IpaSCrst3ayAj07fWJf-O_Un1LmJ4DlSk1O34bv6QAxM',
-  //'lvu5g-8eUhcDZqoH8M2HhtS6HlRy2fglYYgC5aY6tQmOPrenA6N3lNj3Tw',
- // 'u_jUdgjUon8qmoNC5LHJARhEY4jwYGv82dsqpBFIbOFjW6TxkBOgfcmN7Q',
- // 'OyMh80BrEUdzI7t_TjVDl7bQCY04ZNPVN05hgy6RMQaW9Rw',
-  //'OxbgP84fcWdfPGLMdMGjFIR1zL90bARSg0GpSAkzeLMHRvY',
- // 'LPkfX5IlsHyJ8lHN3ZkC2qdQM_N9wdY9bWLUQ_bLTLn8gUM',
-  //'Wz7W2ylKuCH0Lwpx2CgRnvURVepz6nlidzTKtArfNKKT-M',
-  //'Q92MY2imzhNMsOHhkbGUOwRVZ8kpZwn6IjAbFJc1k_Qw1hk',
-  //'_qxJ8twjqXgor4DNZXwrFFfRQrMHCoUaNBaOy8LWQDC87dQ',
- // 'Pxj8wk-DX7ZkSs4Gak1frllaEfPIYpfhUp8JjcQuw46pgsqf'
+  'z4tizn8FKl_bZHhg515w50lB8FJaw-v563thdvYUKaEY3nbbr-JaJ61o_g', //Camaron Es Payo
+  '2ofONtAVY8MXavuXSYbSRDVjO_UYTm9DSeCB_mIPUMiioUsrHd7H7mxvSA', //CokMarioCok
+  'IsXmN3liQ9YQw_--VN-_vk97TqESnnnebXL9TaWiOeRg2xZ2zDJkMysrwg', //Biyín88
+  'F-5b6ZFPAPUQYI3WibmQhoZzv_l0WWxBK2c-LXXH4nvI0LiNTXG0ynVMGQ' //Nampa de Matola
 ];
 
 // Valor por defecto para el orden
