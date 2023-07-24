c11eccca4183a92cde50e142cbc2b840700be1b4
M-pozo <117214588+M-pozo@users.noreply.github.com>
2023-07-24 13:58:47 +0200

    Update 11S Pilot

--- connectToApi.js
+++ connectToApi.js
@@ -10,7 +10,8 @@ const participantes = [
   'huJR-495PKHeCeLcIqI9lT1Roy0VTwWWaWwAokVoSZX4YhdIWZjkU2oX7A', //ShineGang
   'zlSMd_xkgn5G5vYi8SlY7HeHdtjaKWJZIknKfSeekWfIgl7iFZhRV_MpWA', //EvelynnToilet
   '6LEmPnkaBoIqL7vOGxHuB4cKcpOIOTB7c9xehdknbyKw0PRLaN9RXaU9Fg', //Elashaellituy
-  'qc4bew51P1ueV29M6ZOnbl2AKcMdpexY3WG548HtbW-vS61w9WX0bUK6mg' //Riustickyley
+  'qc4bew51P1ueV29M6ZOnbl2AKcMdpexY3WG548HtbW-vS61w9WX0bUK6mg', //Riustickyley
+  'HN7pFPpYKEsC5V9LlejUu1B6DujyLfYO7TZXrbPe_kD0cOoh7uKAwSYvhw' //11S Pilot
 ];
 
 // Valor por defecto para el orden
