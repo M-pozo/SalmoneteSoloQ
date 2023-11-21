2b729e24814099f84e07fe1ef96b7971fe23a1a6
M-pozo <117214588+M-pozo@users.noreply.github.com>
2023-11-21 23:25:56 +0100

    Martin

--- connectToApi.js
+++ connectToApi.js
@@ -86,6 +86,18 @@ async function generarTabla(orden) {
         <td>${((participante.wins * 100) / (participante.wins + participante.losses)).toFixed(0)}%</td>
         <td><a href="https://www.op.gg/summoners/euw/${participante.summonerName}" target="_blank">opgg</a></td>
       `;
+      }else if (participante.summonerName === "winstars"){
+        fila.innerHTML =`
+        <td>${contador++}. ${participante.summonerName}</td>
+        <td><img src="img/usuario/${participante.summonerName}.png" style="width: 30px; height: 30px;"/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src="img/posicion/${participante.summonerName}.png" style="width: 30px; height: 30px;"/></td>
+        <td><img src="img/elo/${participante.tier}${participante.rank}.png" style="width: 30px; height: 30px;"/>EMERALD I</td>
+        <td><font></font>3 LP</td>
+        <td>${participante.wins + participante.losses}</td>
+        <td><font color="green">${participante.wins}</font></td>
+        <td><font color="red">${participante.losses}</font></td>
+        <td>65%</td>
+        <td><a href="https://www.op.gg/summoners/euw/${participante.summonerName}" target="_blank">opgg</a></td>
+      `;
       }else{
         fila.innerHTML =`
         <td>${contador++}. ${participante.summonerName}</td>
